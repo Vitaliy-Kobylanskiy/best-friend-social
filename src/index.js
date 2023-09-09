5d9b2f08c8d1c953a1a89989074f4ee957d6b1e1
vitkoby <vitkoby29@gmail.com>
2023-09-09 04:52:27 +0300

    hash was changed

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
         <Provider store={store} >
             <App />
         </Provider>
