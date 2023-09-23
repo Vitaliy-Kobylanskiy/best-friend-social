cd9b67f442d9f53b58b1f4ae26b3b2ab9dca8f3d
vitkoby <vitkoby29@gmail.com>
2023-09-23 04:40:55 +0300

    logo link has been deleted

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -5,12 +5,10 @@ import cl from './Logo.module.scss';
 const Logo = () => {
     return (
         <div className={cl.block}>
-            <a className={cl.link} href="#">
-                <div className={cl.wrapper}>
-                    <img className={cl.logo} src={headerLogo} alt="logo" />
-                    <h2 className={cl.title}>Best Friends</h2>
-                </div>
-            </a>
+            <div className={cl.wrapper}>
+                <img className={cl.logo} src={headerLogo} alt="logo" />
+                <h2 className={cl.title}>Best Friends</h2>
+            </div>
         </div>
     );
 };
